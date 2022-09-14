10f15fbd254a9e1ec821e1f1d87839bcf74224e8
Mateus-Sousa23 <mateusmartinsipu2@gmail.com>
2022-09-14 09:21:48 -0300

    [add]: frontend React init

---
+++ web/src/App.tsx
@@ -0,0 +1,28 @@
+// Components / Propriedades
+
+interface ButtonProps {
+    title: string;
+}
+
+function Button(props: ButtonProps) {
+    return (
+	<button>
+	  {props.title} 
+	</button>
+    )
+
+}
+
+
+
+function App() {
+    return (
+	<div>
+	  <Button title="Send 1" />
+	  <Button title="Send 2" />
+      <Button title="Send 3"  />
+	</div>
+    )
+}
+
+export default App;
