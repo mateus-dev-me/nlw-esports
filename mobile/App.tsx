223336bff451cf3a033609fd221d75467b383c2d
Mateus-Sousa23 <mateusmartinsipu2@gmail.com>
2022-09-18 15:13:54 -0300

    [add]: react native components

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,39 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, ButtonProps } from 'react-native';
+import { useEffect, useRef } from "react";
+import { StatusBar } from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+} from '@expo-google-fonts/inter'
+import { Subscription } from 'expo-modules-core';
+import * as Notifications from 'expo-notifications';
 
-interface ButttonProps {
-  title: string
-}
+import { Routes } from './src/routes';
+import { Background } from "./src/components/Background";
+import { Loading } from "./src/components/Loading";
+
+import './src/service/notificationConfigs';
+import { getPushNotificationToken } from "./src/service/getPushNotificationToken";
 
 export default function App() {
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken()
+  }, []);
+
+  useEffect(() => {
+    getNotificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      console.log(notification)
+    });
+
+    responseNotificationListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response)
+    });
+
+    return () => {
+      if (getNotificationListener.current && responseNotificationListener.current) {
+        Notifications.removeNotificationSubscription(getNotificationListener.current);
+        Notifications.removeNotificationSubscription(responseNotificationListener.current);
+      }
+    }
+  }, []);
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black,
+  });
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Feliz dia do Programador!</Text>
-      <StatusBar style="auto" backgroundColor='red'/>
-      <Button title='Send 1'/>
-    </View>
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {fontsLoaded ? <Routes /> : <Loading />}
+    </Background>
   );
 }
 
-function Button(props: ButtonProps) {
-  return (
-    <TouchableOpacity>
-      <Text>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-}
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 24
-  }
-});
